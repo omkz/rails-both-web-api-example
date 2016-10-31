655b3c324590c775363af19ab56301c7ad785adc
kurnia muhamad <omenkzz@gmail.com>
2016-10-31 11:40:58 +0700

    add some seeds to populate the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.create(email: 'omz@gmail.com', password: 'rahasia')
+u2 = User.create(email: 'omx@gmail.com', password: 'rahasia')
+
+p1 = u1.posts.create(title: 'First Post', body: 'An Airplane')
+p2 = u1.posts.create(title: 'Second Post', body: 'A Train')
+
+p3 = u2.posts.create(title: 'Third Post', body: 'A Truck')
+p4 = u2.posts.create(title: 'Fourth Post', body: 'A Boat')
+
+p3.comments.create(body: "This post was terrible", user: u1)
+p4.comments.create(body: "This post was the best thing in the whole world", user: u1)
