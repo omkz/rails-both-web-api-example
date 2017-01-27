e66230b6a15d19d317012255891e969043200533
kurnia muhamad <omenkzz@gmail.com>
2017-01-28 01:54:44 +0700

    versioning the API

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
-  resources :users
+
+  namespace :api do
+    namespace :v1 do
+      resources :comments
+      resources :posts
+      resources :users
+    end
+  end
 
   get :token, controller: 'application'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
